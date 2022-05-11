168e83ec173276e65743a8e7af4d59a509b0afd8
awb715 <aaronbellwebdev@gmail.com>
2022-05-10 22:15:37 -0600

    creation of community health sql string using f strings

--- sql_queries.py
+++ sql_queries.py
@@ -1,15 +1,20 @@
+comm_inputs = {
+    'days':40,
+    'channel_id':'840982271309250590',
+    'discord_role_id':'846796122907475978',
+    'table_request':'inactive_table'
+}
 
-
-community_health_query ="""/* QUERY MESSAGES TABLE WITHIN PERIOD*/
+community_health_query = f"""/* QUERY MESSAGES TABLE WITHIN PERIOD*/
 with active_user_messages as (
 SELECT m.channel_id,m.channel_name, m.TIMESTAMP, d.username as user,d.discord_user_id, CURRENT_DATE - m.TIMESTAMP as activity_period FROM discord_messages m
 JOIN discord_user d ON d.discord_user_id = m.author_user_id 
 WHERE 
-/*PYTHON PARAMETER - ACTIVTY PERIOD */
-m.timestamp > CURRENT_DATE - 60
+/*PYTHON PARAMETER - ACTIVITY PERIOD */
+m.timestamp > CURRENT_DATE - {comm_inputs['days']}
 AND 
 /*PYTHON PARAMETER - WHERE STATEMENT FOR CHANNEL*/
-m.channel_id = '840982271309250590'
+m.channel_id = {comm_inputs['channel_id']}
 ),
 
 /* GET USERS WITH SPECIFIC ROLE (role is active) */
@@ -22,10 +27,12 @@ WHERE
 r.active = True
 AND 
 /*PYTHON PARAMETER  - WHERE STATEMENT FOR ROLE*/
-dr.role_name = 'Analytics Guild'
+dr.discord_role_id = {comm_inputs['discord_role_id']}
 ),
 
-/*Users with role who are active in activity period*/
+
+
+/*USERS WITH ROLE IN SPECIFIC PERIOD*/
 active_table as (
 SELECT u1.channel_name, u1.user, min(u1.activity_period) as days_since_active FROM active_user_messages u1
 INNER JOIN users_with_roles u2 ON u1.discord_user_id = u2.discord_user_id
@@ -33,29 +40,35 @@ GROUP BY 1,2
 ORDER BY 3
 ),
 
+
+/* QUERY MESSAGES TABLE OUTSIDE OF PERIOD*/
 inactive_user_messages as (
 SELECT m.channel_id,m.channel_name, m.TIMESTAMP, d.username as user,d.discord_user_id, CURRENT_DATE - m.TIMESTAMP as activity_period FROM discord_messages m
 JOIN discord_user d ON d.discord_user_id = m.author_user_id 
 WHERE 
 /*PYTHON PARAMETER - ACTIVTY PERIOD */
-m.timestamp < CURRENT_DATE - 60
+m.timestamp < CURRENT_DATE - {comm_inputs['days']}
 AND 
 /*PYTHON PARAMETER - WHERE STATEMENT FOR CHANNEL*/
-m.channel_id = '840982271309250590'
+m.channel_id = {comm_inputs['channel_id']}
 ),
 
+
 /*Users with role who are inactive in activity period*/
-inactive_table as (
-SELECT u4.channel_name,u3.username, min(u4.activity_period) ,'Analytics' as Channel from users_with_roles u3
+
+inactive_table as (SELECT u4.channel_name,u3.username, min(u4.activity_period) as days_since_active from users_with_roles u3
 LEFT JOIN inactive_user_messages u4 ON u3.discord_user_id = u4.discord_user_id
 WHERE u3.username NOT IN (
-                          SELECT user from active_table GROUP BY 1
-                          /* Why is this not working?*/
+                          SELECT a.user from active_table a GROUP BY 1
                           )
+AND 
+u4.channel_name IS NOT NULL
 GROUP BY 1,2
 ORDER BY 3
+
 )
 
-SELECT * from active_table """
+select * from {comm_inputs['table_request']}
+"""
 
 print(community_health_query)
