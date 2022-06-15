5dce9f1971df12e44ab500cf7674d7213ed4719c
awb715 <aaronbellwebdev@gmail.com>
2022-06-14 22:06:19 -0600

    handling of no discord roles in sql query string creation

--- sql_queries.py
+++ sql_queries.py
@@ -1,16 +1,40 @@
 
 
 def create_health_query(comm_inputs):
-
+    
     if len(comm_inputs['discord_role_ids']) >0:
+
+        submitted_roles = comm_inputs['discord_role_ids'].split(',')
+        print(submitted_roles)
+        query_roles=[]
+        i=1
+        print(str(len(submitted_roles)) + 'lenght')
+        for r in submitted_roles:
+            print(len(submitted_roles))
+    
+            if i == len(submitted_roles):
+                print(str(i)+ ' i first')
+                
+                print(i, r)
+                query_roles.append('\''+r.strip())
+                role_string = ''.join(query_roles)
+                
+            else:
+                print(i, r)
+                print(str(i)+ ' second')
+                query_roles.append('\''+r.strip()+'\',')
+                i+=1 
+                
+        role_string = ''.join(query_roles)  + '\'' 
+        print(role_string)
+            
+
         append_string = f"""AND 
     /*PYTHON PARAMETER  - WHERE STATEMENT FOR ROLE*/
-    dr.role_name IN ('{comm_inputs['discord_role_ids']}')
-    )'"""
+    dr.role_name IN ("""+role_string+f""")
+    """
     else:
         append_string=''
-
-
 
 
     return  f"""/* QUERY MESSAGES TABLE WITHIN PERIOD*/
@@ -33,16 +57,11 @@ def create_health_query(comm_inputs):
     JOIN discord_user du ON du.discord_user_id = r.discord_user_id
     WHERE
     r.active = True
-    AND 
-    /*PYTHON PARAMETER  - WHERE STATEMENT FOR ROLE*/
-    dr.role_name IN ('{comm_inputs['discord_role_ids']}')
-    ),
-
-
+    """+append_string+f"""),
 
     /*USERS WITH ROLE IN SPECIFIC PERIOD*/
     active_table as (
-    SELECT u1.channel_name, u1.user, min(u1.activity_period) as days_since_active, 'Active Member Analysis' as report_name FROM active_user_messages u1
+    SELECT u1.channel_name, u1.user, floor(min(u1.activity_period)) as days_since_active, 'Active Member Analysis' as report_name FROM active_user_messages u1
     INNER JOIN users_with_roles u2 ON u1.discord_user_id = u2.discord_user_id
     GROUP BY 1,2
     ORDER BY 3
@@ -64,7 +83,7 @@ def create_health_query(comm_inputs):
 
     /*Users with role who are inactive in activity period*/
 
-    inactive_table as (SELECT u4.channel_name,u3.user as user, min(u4.activity_period) as days_since_active,'Inactive Member Analysis' as report_name from users_with_roles u3
+    inactive_table as (SELECT u4.channel_name,u3.user as user, floor(min(u4.activity_period)) as days_since_active,'Inactive Member Analysis' as report_name from users_with_roles u3
     LEFT JOIN inactive_user_messages u4 ON u3.discord_user_id = u4.discord_user_id
     WHERE u3.user NOT IN (
                             SELECT a.user from active_table a GROUP BY 1
