343ec784a3cc763d12c906dfbe9d14c097c889e7
awb715 <aaronbellwebdev@gmail.com>
2022-07-05 22:36:50 -0600

    add help embed command for 2 queries

--- daodash_commands_bot.py
+++ daodash_commands_bot.py
@@ -7,6 +7,8 @@ import datetime
 from dependencies import token 
 from get_data import *
 
+from commands_help import *
+
 
 bot = commands.Bot(command_prefix='!')
 bot.remove_command('help')
@@ -60,34 +62,33 @@ async def members(ctx,*args):
         params =  ','.join(args)
         print(len(args))
         
-        ##print(ctx.channel.id)
 
-        ##argument 2 will not exist if roles aren't specified
-        if len(args)>2:
-            dri = args[2]
+        if args[0] == 'help':
+            await ctx.send(embed=member_help_embed)
         else:
-            dri = ''
+            ##argument 2 will not exist if roles aren't specified
+            if len(args)>2:
+                dri = args[2]
+            else:
+                dri = ''
 
-        print (dri)
-        ##object of values used in querying
-        obj = {
-    'days':args[1],
-    'channel_id':'840982271309250590',
-    'discord_role_ids':dri,
-    'table_request':args[0]+'_table',
-    'start_time':start_time,
-    'user':ctx.author.name,
-    
-}
+            print (dri)
+            ##object of values used in querying
+            obj = {
+            'days':args[1],
+            'channel_id':'840982271309250590',
+            'discord_role_ids':dri,
+            'table_request':args[0]+'_table',
+            'start_time':start_time,
+            'user':ctx.author.name,        
+            }
 
-        ##function which kicks off db querying, chart creation
-        community_health(obj)
+            ##function which kicks off db querying, chart creation
+            community_health(obj)
 
-        filename = 'CommunityHealth - '+start_time +'.png'
-
-
-    ##bot sends message with file attached
-        await ctx.send('Hey @'+str(ctx.author)+', Here is your requested chart \n\n',file=discord.File(r"images/"+filename))
+            filename = 'CommunityHealth - '+start_time +'.png'
+        ##bot sends message with file attached
+            await ctx.send('Hey @'+str(ctx.author)+', Here is your requested chart \n\n',file=discord.File(r"images/"+filename))
 
 @bot.command()
 async def multisig(ctx,*args):
@@ -95,21 +96,24 @@ async def multisig(ctx,*args):
      start_time = str(datetime.datetime.now()).replace(":",".")
      filename = 'Multisig - '+start_time +'.csv'
 
-     if len(args)>1:
-            sd = args[1]
+
+     if args[0]=='help':
+        await ctx.send(embed=multisig_help_embed)
      else:
-            sd = False
+        if len(args)>1:
+                sd = args[1]
+        else:
+                sd = False
 
-     obj = {
-    'wallet':args[0],
-    'start_date':sd,
-    'filename':filename,
-    'user':ctx.author.name
-    }
+        obj = {
+        'wallet':args[0],
+        'start_date':sd,
+        'filename':filename,
+        'user':ctx.author.name
+        }
 
-     multisig_analysis(obj)
+        multisig_analysis(obj)
 
-
-     await ctx.send('Hey @'+str(ctx.author)+', Here is your requested data.',file=discord.File(r"data/"+filename))
+        await ctx.send('Hey @'+str(ctx.author)+', Here is your requested data.',file=discord.File(r"data/"+filename))
 
 bot.run(token)
