1d2d10efebe35a247063bfc07cd100787dd99ec7
awb715 <aaronbellwebdev@gmail.com>
2022-07-05 22:17:34 -0600

    clean up embed

--- daodash_commands_bot.py
+++ daodash_commands_bot.py
@@ -17,37 +17,38 @@ async def daodash(ctx):
 
     embed=discord.Embed(
         title='DAO Dash Help',
-        description='DAO Dash can help with your analytics.',
+        description="""DAODash offers insights about your DAO’s health right here in Discord. 
+
+See below for the commands DAODash accepts. 
+
+Please enter any command followed by help to get more information on the command.
+
+Example: !members help
+""",
         color = discord.Colour.red()
     )
 
     embed.add_field(
-        name='!help',
-        value='returns all dashbot commands currently available.',
-        inline=True
-    )
-
-    # embed.add_field(
-    #     name='!chart_list',
-    #     value='returns a list of all charts currently available for that server.',
-    #     inline=False
-    # )
-
-
-    embed.add_field(
         name='!members',
-        value='List of Discord users who have been <in>active in the respective channel in the last <x> days.  Sorted by the most recently active user, bar chart indicates number of days inactive\n\n'+
-        'Parameters: members [-inactive/active] [activity_threshold] [roles]\n '+
-        '[inactive/active] - Ex: Active (String)\n'+
-         '[activity_threshold] - Number of days '+
-         '\n[discord_role(s)] - List out roles in quotes'
-         '\n\nExample: !members active 10 "analytics_guild, guest pass"',
+        value='returns a sorted list of active or inactive members in channel.',
         inline=False
     )
 
     embed.add_field(
         name='!multisig',
-        value='List of wallet addresses that received <token> from <multisig address> in the last <x> days. (flow thickness is proportional to amount of tokens transferred)',
+        value="""returns the wallets and amounts of DAO tokens that were distributed from a specific
+multisig address""",
+        inline=False
+    )
+
+    embed.add_field(
+        name='!snapshot',
+        value='returns metrics around community engagement in the last X votes',
+        inline=False
+    )
+    embed.add_field(
+        name='!roles',
+        value='returns data around users acquiring a specific DAO Discord role',
         inline=False
     )
 
