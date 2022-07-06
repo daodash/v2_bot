f7a587206d8a146c925aa13be180d52fe76e2090
awb715 <aaronbellwebdev@gmail.com>
2022-07-06 12:21:13 -0600

    creation of sankey chart image

--- charts.py
+++ charts.py
@@ -1,6 +1,8 @@
     # %%
 import pandas as pd
 import plotly.express as px
+import numpy as np
+import plotly.graph_objects as go
 
 
 
@@ -75,3 +77,36 @@ def health_bar_chart(df,obj):
 # }
 
 # health_bar_chart(df,obj)
+
+def multisig_sankey(df,obj):
+
+    ##set endpoints (from address to user)
+    nodes = np.unique(df[["from_address","user"]],axis=None)
+    nodes = pd.Series(index=nodes, data=range(len(nodes)))
+
+    ##build sankey chart
+    fig =  go.Figure(
+            go.Sankey(
+                node={"label": nodes.index},
+                link={
+                    "source": nodes.loc[df["from_address"]],
+                    "target": nodes.loc[df["user"]],
+                    "value": df["sum"],
+                },
+            )
+        )
+    ##write image
+    print(obj['filename'])
+    fig.write_image(r"images/"+obj['filename'])
+
+
+    ##testing for multisig
+# df = pd.read_csv('Multisig_-_2022-07-05_22.42.23.542698.csv')
+
+# obj3=  {
+#         'wallet':'0xe7636c7ef670a3Bcf772D9d57244c9e88aD90437',
+#         'start_date':'2022-01-01',
+#         'filename':'testmultisig.png',
+#         'user':'aaron'
+#         }
+# multisig_sankey(df,obj3)
