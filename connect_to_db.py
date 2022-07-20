91a1a62c685ae5f052701ff7d5bdec57beb8b6bb
awb715 <aaronbellwebdev@gmail.com>
2022-07-20 13:36:00 -0600

    add .env

--- connect_to_db.py
+++ connect_to_db.py
@@ -2,13 +2,25 @@
 from dependencies import creds
 import psycopg2
 import pandas as pd
+import os
 
+##access env variables
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+print(os.getenv('USER'))
 ##print(creds)
 
 def db_query(sql_string):
 #connect to database
     con = psycopg2.connect(    
-  
+    host = os.getenv('HOST'),
+    database= os.getenv('DATABASE'),
+    user = os.getenv('USER'),
+    password = os.getenv('PASSWORD'),
+    port = os.getenv('PORT'),
+    sslmode =os.getenv('SSLMODE')
     )
 
     #create cursor
